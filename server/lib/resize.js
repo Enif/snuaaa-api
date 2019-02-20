847c4d1724c6c6eb48946dcc56b093a07626f984
YOUNGJAE KIM <yjkim1803@gmail.com>
2019-02-21 00:19:28 +0900

    make resize promise function

--- server/lib/resize.js
+++ server/lib/resize.js
@@ -2,33 +2,36 @@ const fs = require('fs');
 const sharp = require('sharp');
 
 exports.resize = function (path) {
-    console.log('[resize]path ', path);
-    fs.readFile(path, function (err, data) {
-        console.log('[resize]err ', err);
-        if (err) {
-            console.log('[resize]err ', err);
-        } else {
-            const image = sharp(data)
 
-            image.metadata()
-            .then(function(metadata) {
-                console.log('meta >> ', metadata);
-                if(metadata.size > 1024 * 1024) {
-                    if(metadata.height > metadata.width) {
-                        return image.resize({ width : 720}).toFile(path)
+    return new Promise((resolve, reject) => {
+
+        fs.readFile(path, function (err, data) {
+            if (err) {
+                reject(err)
+            }
+            else {
+                const image = sharp(data)
+                image.metadata()
+                .then(function(metadata) {
+                    if(metadata.size > 1024 * 1024) {
+                        if(metadata.height > metadata.width) {
+                            return image.resize({ width : 720}).toFile(path)
+                        }
+                        else {
+                            return image.resize({ height : 720}).toFile(path)
+                        }
                     }
                     else {
-                        return image.resize({ height : 720}).toFile(path)
+                        resolve();   
                     }
-                }
-                else return;
-            })
-            .then(function (info) {
-                console.log('resize success');
-                console.dir(info);
-            }).catch(function (err) {
-                console.log('failed to resize');
-            });
-        }
+                })
+                .then(function (info) {
+                    resolve(info);
+                })
+                .catch(function (err) {
+                    reject(err)
+                });
+            }
+        });
     });
 };

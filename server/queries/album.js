7b1024810f45f88d4eb3c07ea2f5e73dd508ec4a
YOUNGJAE KIM <yjkim1803@gmail.com>
2019-03-25 23:43:40 +0900

    make retrieveAlbumsByCategory api

--- server/queries/album.js
+++ server/queries/album.js
@@ -8,7 +8,7 @@ exports.retrieveAlbums = function(board_id) {
         }
         else {
             let query = `
-                SELECT al.object_id, ob.title, ob.contents, ob.created_at, usr.nickname,
+                SELECT al.object_id, ob.category_id, ob.title, ob.contents, ob.created_at, usr.nickname, ctg.category_color,
                 (
                     SELECT ph.file_path
                     FROM snuaaa.tb_photo ph
@@ -20,6 +20,7 @@ exports.retrieveAlbums = function(board_id) {
                 FROM snuaaa.tb_album al
                 INNER JOIN snuaaa.tb_object ob ON (al.object_id = ob.object_id)
                 INNER JOIN snuaaa.tb_user usr ON (ob.author_id = usr.user_id)
+                LEFT OUTER JOIN snuaaa.tb_category ctg ON (ob.category_id = ctg.category_id)
                 WHERE ob.board_id = $1
             `;
             db.any(query, board_id)
@@ -28,6 +29,43 @@ exports.retrieveAlbums = function(board_id) {
             })
             .catch((err) => {
                 console.error(err)
+                reject(err)
+            })
+        }
+    })
+};
+
+exports.retrieveAlbumsbyCategory = function(board_id, category_id) {
+    console.log('retrieve by category..')
+    return new Promise((resolve, reject) => {
+        if(!board_id) {
+            reject();
+        }
+        else {
+            let query = `
+                SELECT al.object_id, ob.category_id, ob.title, ob.contents, ob.created_at, usr.nickname, ctg.category_color,
+                (
+                    SELECT ph.file_path
+                    FROM snuaaa.tb_photo ph
+                    INNER JOIN snuaaa.tb_object phobj ON (phobj.object_id = ph.object_id)
+                    WHERE ph.album_id = al.object_id
+                    ORDER BY phobj.created_at DESC
+                    LIMIT 1
+				)
+                FROM snuaaa.tb_album al
+                INNER JOIN snuaaa.tb_object ob ON (al.object_id = ob.object_id)
+                INNER JOIN snuaaa.tb_user usr ON (ob.author_id = usr.user_id)
+                LEFT OUTER JOIN snuaaa.tb_category ctg ON (ob.category_id = ctg.category_id)
+                WHERE ob.board_id = $1
+                AND ob.category_id = $2
+            `;
+            db.any(query, [board_id, category_id])
+            .then(function(albums){
+                resolve(albums);    
+            })
+            .catch((err) => {
+                console.error(err)
+                console.log(err)
                 reject(err)
             })
         }

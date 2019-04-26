485ffa0ce9195b070557fb669856eb3b1dff4582
최규민 <choigyumin@gmail.com>
2019-04-26 17:50:54 +0900

    signup-as-many-as-possible

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,7 +12,8 @@ import photo from './photo';
 import document from './document';
 import soundbox from './soundbox';
 import object from './object';
-import comment from './comment'
+import comment from './comment';
+import user from './user';
 // import profile from './profile';
 
 const router = express.Router();
@@ -29,7 +30,9 @@ router.use('/photo', photo);
 router.use('/document', document);
 router.use('/soundbox', soundbox);
 router.use('/object', object);
-router.use('/comment', comment)
+router.use('/comment', comment);
+router.use('/user', user);
+
 // router.use('/profile', profile);
 
 export default router;

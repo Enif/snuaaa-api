2fb6cb6fcfd45e29d96c4dd8ea03c5d569ed14f3
yjkim <yjkim1803@gmail.com>
2021-11-20 22:40:28 +0900

    fix: update corsOption

--- server/main.js
+++ server/main.js
@@ -21,33 +21,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-if(process.env.NODE_ENV == 'develop') {
+if (process.env.NODE_ENV == 'develop') {
     // for local test
-    app.use(cors())
-    app.use(express.static(__dirname + '/../../snuaaa-react/build'));
-    app.get('/page/*', function (req, res) {
-        res.sendFile(path.join(__dirname, '/../../snuaaa-react/build/index.html'));
-    });
+    app.use(cors());
 }
 else {
-    // [TODO] SET CORS OPTIONS AFTER PUBLISHING
-    var corsOptions = {
-        origin: 'https://www.snuaaa.net',
+    const corsOptions = {
+        origin: ['https://www.snuaaa.net', 'https://community.snuaaa.net'],
         optionsSuccessStatus: 200
     }
-    app.use(cors(corsOptions))
-    app.use(express.static(path.join(__dirname, '/../build-react')));
-    app.get('/page/*', function (req, res) {
-        res.sendFile(path.join(__dirname, '/../build-react/index.html'));
-    });
+    app.use(cors(corsOptions));
 }
 
 app.use('/static', express.static(__dirname + '/../upload'));
 
-
-
 // [CONFIGURE SERVER PORT]
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 // [RUN SERVER]
 app.listen(port, () => console.log(`Server listening on port ${port}`));
